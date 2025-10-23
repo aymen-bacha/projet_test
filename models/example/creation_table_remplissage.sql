f5f29efdc69f8067e4cb3674bc07a0d386a96aaf
aymen-bacha <bachaaymen2007@yahoo.fr>
2025-10-23 16:23:17 +0200

    creation et remplissage de la table personne

---
+++ models/example/creation_table_remplissage.sql
@@ -0,0 +1,12 @@
+CREATE TABLE personne (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(50),
+    prenom VARCHAR(50)
+);
+INSERT INTO personne (nom, prenom)
+VALUES 
+('bacha', 'aymen'),
+('bacha', 'wael'),
+('soula', 'wadia')
+('berrajeh', 'mohamed');
+
