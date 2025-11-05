3cf82c89ecb4bd3437a7b79133ff3da3873bd1e4
aymen-bacha <bachaaymen2007@yahoo.fr>
2025-11-05 20:49:03 +0100

    derniere correction

--- models/example/ville.sql
+++ models/example/ville.sql
@@ -6,5 +6,5 @@ FROM (
         (1, 'Puteaux', '92800'),
         (2, 'Argenteuil', '95100'),
         (3, 'Paris', '75000'),
-	(4, 'Medenine' '4100')
+	(4, 'Medenine', '4100')
 ) AS t(id, nom, code postal)
