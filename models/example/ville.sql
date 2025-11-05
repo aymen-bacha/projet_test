ce2a979e4e6a14d945e3c0a3ff62cd8cdbf48d94
aymen-bacha <bachaaymen2007@yahoo.fr>
2025-11-05 20:09:53 +0100

    rajout d'une nouvelle ville

--- models/example/ville.sql
+++ models/example/ville.sql
@@ -5,5 +5,6 @@ FROM (
     VALUES 
         (1, 'Puteaux', '92800'),
         (2, 'Argenteuil', '95100'),
-        (3, 'Paris', '75000')
+        (3, 'Paris', '75000'),
+	(4, 'Medenine' '4100')
 ) AS t(id, nom, code postal)
