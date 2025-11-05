8325b4942e9708ff1633e8db6a7ab9188c2d0e11
aymen-bacha <bachaaymen2007@yahoo.fr>
2025-11-05 06:36:52 +0100

    nouveau changement

---
+++ models/example/ville.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='table') }}
+
+SELECT *
+FROM (
+    VALUES 
+        (1, 'Puteaux', '92800'),
+        (2, 'Argenteuil', '95100'),
+        (3, 'Paris', '75000')
+) AS t(id, nom, code postal)
