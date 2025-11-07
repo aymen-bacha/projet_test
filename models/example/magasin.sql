1e5fd5b7ee2390965f75af43a00059c9ea5f70f7
aymen-bacha <bachaaymen2007@yahoo.fr>
2025-11-07 09:32:06 +0100

    essaie 10

--- models/example/magasin.sql
+++ models/example/magasin.sql
@@ -4,5 +4,6 @@ SELECT *
 FROM (
     VALUES
         (1, 'aldi', 'puteaux'),
-        (2, 'monoprix', 'suresnes')
+        (2, 'monoprix', 'suresnes'),
+	(3, 'lidl', 'medenine')
 ) AS t(id, nom, ville)
